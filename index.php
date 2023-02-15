075c9cd38301300e9d240f7aa2d24a3e4810fbc3
Anji0902 <angabai18@gmail.com>
2023-02-15 14:07:32 +0200

    added index home page

--- index.php
+++ index.php
@@ -1,61 +1,4 @@
-
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Final Project</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
-  </head>
-  <body>
-    <div class="container">
-
-        <div class="row">
-            <div class="col-md-2" style="padding-top: 15px;">
-              <img class="img-fluid" src="images/logonew.jpg" alt="logo" style="height: 120px; width: 220px; " > 
-            </div>
-            <div class="col-md-10"> 
-
-    <h1>We bring the taste of Srilankan foods to Suomi </h1>
-
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-          
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active" href="index.html">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link active" href="#">Order Online</a>
-              </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link active dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Our Menu
-                </a>
-                <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="srilankan.html">Srilankan</a></li>
-                  <li><a class="dropdown-item" href="indian.html">Indian</a></li>                  
-                  <li><a class="dropdown-item" href="chinese.html">Chinese</a></li>
-                </ul>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link active" href="#">Gallery</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link active" href="contact.html">Contact us</a>
-              </li>
-            </ul>
-            <form class="d-flex" role="search">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Search</button>
-              </form>
-            </div>
-        </div>
-      </nav>
-    </div>
-</div>
+<?php include "header.php"; ?>
 
 <div class="row">
     <div class="col-md-6" style="font-size: 65px; font-weight: bolder; text-align: center; font-family:cursive; color: aliceblue; padding-top: 50px;">
@@ -188,3 +131,5 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
   </body>
 </html>
+
+<?php include "footer.php" ?>
