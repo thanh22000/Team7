1a1aee0309fe4d4f233293c82b04445eafa3f25d
anuradha220001 <anuradha22000@student.hamk.fi>
2023-02-15 18:04:14 +0200

    adding images and gallery page

---
+++ gallery.php
@@ -0,0 +1,69 @@
+<?php
+include 'header.php';
+?>
+
+<br>
+
+<h2>GALLERY</h2 style="text-align:center; "><br>
+
+    <div class="gallery">
+        <div class="card" >
+            <img src="images/gallery4.jpg" alt="pittu" style="height: 550px; width: 400px;">
+                 </div>
+    
+        <div class="card" >
+            <img src="images/gallery5.jpg" alt="stringhoppers" style="height: 550px; width: 400px;">
+            
+            </div>
+    
+        <div class="card" >
+            <img src="images/gallery8.jpg" alt="idly" style="height: 550px; width: 400px;">
+            
+            </div>
+        </div><br>
+
+        <div class="gallery">
+            <div class="card" >
+                <img src="images/gallery1.jpg" alt="roti" style="height: 250px; width: 400px;">
+                
+                </div>
+        
+            <div class="card" >
+                <img src="images/gallery6.jpg" alt="hoppers" style="height: 250px; width: 400px;">
+                
+                </div>
+        
+            <div class="card" >
+                <img src="images/gallery12.jpg" alt="ricencurry" style="height: 250px; width: 400px;">
+                
+                </div>
+            </div><br>
+        
+            <div class="gallery">
+                <div class="card" >
+                    <img src="images/gallery7.jpg" alt="pittu" style="height: 550px; width: 400px;">
+                         </div>
+            
+                <div class="card" >
+                    <img src="images/gallery10.jpg" alt="stringhoppers" style="height: 550px; width: 400px;">
+                    
+                    </div>
+            
+                <div class="card" >
+                    <img src="images/gallery11.jpg" alt="idly" style="height: 550px; width: 400px;">
+                    
+                    </div>
+                </div><br>
+            
+
+            <br>
+            <br>
+
+            <?php
+            include 'footer.php';
+            ?>
+            
+            
+
+        
+
