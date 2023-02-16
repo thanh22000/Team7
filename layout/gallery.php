39dda4e70cb93b073d5200637683eb5e69654419
thanh22000 <onnellinentc@gmail.com>
2023-02-16 21:15:28 +0200

    Update gallery.php

--- layout/gallery.php
+++ layout/gallery.php
@@ -2,67 +2,45 @@
 $title = "Gallery";
 include 'header.php';
 ?>
-
 <br>
-
-<h2>GALLERY</h2 style="text-align:center; "><br>
+<h2 style="text-align:center;">GALLERY</h2><br>
 
     <div class="gallery">
         <div class="card" >
-            <img src="../Images/gallery4.jpg" alt="pittu" style="height: 550px; width: 400px;">
-                 </div>
-    
+            <img src="Images/gallery4.jpg" alt="pittu" style="height: 550px; width: 400px;">
+        </div>
         <div class="card" >
-            <img src="../Images/gallery5.jpg" alt="stringhoppers" style="height: 550px; width: 400px;">
-            
-            </div>
-    
+            <img src="Images/gallery5.jpg" alt="stringhoppers" style="height: 550px; width: 400px;">
+        </div>
         <div class="card" >
-            <img src="../Images/gallery8.jpg" alt="idly" style="height: 550px; width: 400px;">
-            
-            </div>
-        </div><br>
+            <img src="Images/gallery8.jpg" alt="idly" style="height: 550px; width: 400px;">
+        </div>
+    </div><br>
+    <div class="gallery">
+        <div class="card" >
+            <img src="Images/gallery1.jpg" alt="roti" style="height: 250px; width: 400px;">
+        </div>
+        <div class="card" >
+            <img src="Images/gallery6.jpg" alt="hoppers" style="height: 250px; width: 400px;">
+        </div>
+        <div class="card" >
+            <img src="Images/gallery12.jpg" alt="ricencurry" style="height: 250px; width: 400px;">
+        </div>
+    </div><br>
+    <div class="gallery">
+        <div class="card" >
+            <img src="Images/gallery7.jpg" alt="pittu" style="height: 550px; width: 400px;">
+        </div>
+        <div class="card" >
+            <img src="Images/gallery10.jpg" alt="stringhoppers" style="height: 550px; width: 400px;">
+        </div>
+        <div class="card" >
+            <img src="Images/gallery11.jpg" alt="idly" style="height: 550px; width: 400px;">
+        </div>
+    </div><br>
+<br><br>
 
-        <div class="gallery">
-            <div class="card" >
-                <img src="../Images/gallery1.jpg" alt="roti" style="height: 250px; width: 400px;">
-                
-                </div>
-        
-            <div class="card" >
-                <img src="../Images/gallery6.jpg" alt="hoppers" style="height: 250px; width: 400px;">
-                
-                </div>
-        
-            <div class="card" >
-                <img src="../Images/gallery12.jpg" alt="ricencurry" style="height: 250px; width: 400px;">
-                
-                </div>
-            </div><br>
-        
-            <div class="gallery">
-                <div class="card" >
-                    <img src="../Images/gallery7.jpg" alt="pittu" style="height: 550px; width: 400px;">
-                         </div>
-            
-                <div class="card" >
-                    <img src="../Images/gallery10.jpg" alt="stringhoppers" style="height: 550px; width: 400px;">
-                    
-                    </div>
-            
-                <div class="card" >
-                    <img src="../Images/gallery11.jpg" alt="idly" style="height: 550px; width: 400px;">
-                    
-                    </div>
-                </div><br>
-            
-
-            <br>
-            <br>
-
-            <?php
-            include 'footer.php';
-            ?>
+<?php include 'footer.php';?>
             
             
 
