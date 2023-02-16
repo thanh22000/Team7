0795f8b6795b1a2dc67f3a4e97050f761e22e4ad
thanh22000 <onnellinentc@gmail.com>
2023-02-16 13:55:18 +0200

    Fixing srilankan page before doing my job

--- srilankan.php
+++ srilankan.php
@@ -10,47 +10,44 @@ include 'layout/header.php'; ?>
             <p> Pittu with Katta sambal and<br>
                 Potato Curry<br>
                 € 9.50 </p>
-            </div>
+        </div>
     
         <div class="card" style="text-align: center; font-weight: bold; background-color: darkgray;">
             <img src="images/stringhoppers.jpeg" alt="stringhoppers" style="height: 250px; width: 350px;">
             <p> String Hoppers with Pol<br>
                 sambal and Dhal Curry<br>
                 € 9.50 </p>
-            </div>
+        </div>
     
         <div class="card" style="text-align: center; font-weight: bold; background-color: darkgray;">
             <img src="images/milkrice.jpeg" alt="idly" style="height: 250px; width: 350px;">
             <p> Milk rice with Katta<br>
                 sambal <br>
                 € 9.50 </p>
-            </div>
-        </div><br>
+        </div>
+    </div><br>
 
-        <div class="menu2">
-            <div class="card" style="text-align: center; font-weight: bold; background-color: darkgray;">
-                <img src="images/roti.jpeg" alt="roti" style="height: 250px; width: 350px;">
-                <p>Pol roti with katta<br>
-                    Sambal<br>
-                    € 9.50 </p>
-                </div>
+    <div class="menu2">
+        <div class="card" style="text-align: center; font-weight: bold; background-color: darkgray;">
+            <img src="images/roti.jpeg" alt="roti" style="height: 250px; width: 350px;">
+            <p>Pol roti with katta<br>
+                Sambal<br>
+                € 9.50 </p>
+        </div>
         
-            <div class="card" style="text-align: center; font-weight: bold; background-color: darkgray;">
-                <img src="images/hoppers.jpeg" alt="hoppers" style="height: 250px; width: 350px;">
-                <p> Hoppers with katta <br>
-                    sambal<br>
-                    € 9.50 </p>
-                </div>
+        <div class="card" style="text-align: center; font-weight: bold; background-color: darkgray;">
+            <img src="images/hoppers.jpeg" alt="hoppers" style="height: 250px; width: 350px;">
+             <p> Hoppers with katta <br>
+                 sambal<br>
+                 € 9.50 </p>
+        </div>
         
-            <div class="card" style="text-align: center; font-weight: bold; background-color: darkgray;">
-                <img src="images/riceandcurry.jpeg" alt="ricencurry" style="height: 250px; width: 350px;">
-                <p> Sri Lankan Rice<br>
-                    and Curry<br>
-                    € 12.00 </p>
-                </div>
-            </div>
-
-
-            <br>
-            <br>
-            <?php include 'layout/footer.php'; ?>
+        <div class="card" style="text-align: center; font-weight: bold; background-color: darkgray;">
+            <img src="images/riceandcurry.jpeg" alt="ricencurry" style="height: 250px; width: 350px;">
+            <p> Sri Lankan Rice<br>
+                 and Curry<br>
+                € 12.00 </p>
+        </div>
+    </div>
+<br><br>
+<?php include 'layout/footer.php'; ?>
