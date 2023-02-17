0e068b91ef55217cd26aac4da16c2aa0125c4576
Anji0902 <angabai18@gmail.com>
2023-02-17 13:45:25 +0200

    Change in contact us area

--- index.php
+++ index.php
@@ -348,56 +348,42 @@ if (isset($_POST['submit'])){
 
 <!-- Contact us -->
 <h2 id="contact" style="text-align:center; color:aliceblue;">CONTACT US</h2><br>
-<div class="row">
-    <div class="col-md-6" style="color: aliceblue; text-align: center; font-size:35px; color: cornflowerblue; font-weight: bold; font-style: italic;  font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;">
-         
-      <p1>Get in touch<br>   
-        <br>     
-    </p1>
-    </div>
-    <div class="col-md-6">         
-        </div>
-    </div>
 
-<div class="row">
-    <div class="col-md-6" style="color: aliceblue; text-align: center; font-size: larger;">
-      <p1>
-        Address : <br>
-        Visamäentie 25<br>
-        13100<br>
-        Hämeenlinna<br>
-        <br>
-        <br>
-        <br>
-        Phone:<br>  
-        +358 41586236<br>
-        <br>
-        <br>
-        <br>
-        Email :<br>
-        srilankanfoods@lanka.fi<br> 
-        <br><br><br><br><br>
-    </p1>
-    </div>
-    <div class="col-md-6" style="color: aliceblue;"> 
-
-        <form action="/action_page.php">
-            <label for="name" >Name*      : </label>
-            <div><input type="text" id="name" name="name" size="45"><br><br></div>
-            <label for="email">Email *    : </label>
-            <div><input type="text" id="email" name="email" size="45"><br><br></div>
-            <label for="subject">Subject : </label>
-            <div><input type="text" id="subject" name="subject" size="45"><br><br></div>
-            <label for="lname">Message* : </label>
-            <div><input type="text" id="msg" name="msg" size="45"><br><br></div>
-            <label for="lname">Phone *: </label>
-            <div><input type="text" id="phone" name="phone" size="45"><br><br></div>
-
-            <input type="submit" value="Submit" style="color:coral; font-weight: bold; background-color: blanchedalmond;">
-          </form>
+    <div class="form-group mb-4" style="color: aliceblue;>
+              <label for="name">Your name*:</label>
+              <input id="name" type="text" name="name" class="form-control" placeholder="Example: Dhanushke Perera" required></input>
+            </div>
+            <div class="form-group my-4" style="color: aliceblue;>
+              <label for="Email">E-mail address*:</label>
+              <input id="Email" type="email" name="email" class="form-control" placeholder="Example: dhanushke22000@student.hamk.fi" required>
+            </div>
+            <div class="form-group my-4" style="color: aliceblue;>
+              <label for="subject">Subject:</label>
+              <input id="subject" type="text" name="subject" class="form-control" required>
+            </div>
+            <div class="form-group my-4" style="color: aliceblue;>
+              <label for="subject">Phone Number*:</label>
+              <input id="subject" type="text" name="subject" class="form-control" required>
+            </div>
+            <div class="form-group my-4" style="color: aliceblue;>
+              <label for="details">Details*:</label>
+              <textarea id="details" name="body" class="form-control" rows="4" placeholder="Message." required></textarea>
+            </div>
+            <div class="form-group my-4" style="color: aliceblue;>
+              <button type="submit" class="btn btn btn-yellow btn-outline" >
+              <input type="submit" value="Submit" style="color:coral; font-weight: bold; background-color: blanchedalmond;" class="btn btn btn-yellow btn-outline">
+            <div>
           
-        
-        </div>
+    </div>
+  </div>
+</div>
+        </form>
+      </div>
+
+          
+      </main>
+      
+        <div id="wrap-footer"></div>
     </div>
 
 <?php include 'footer.php'; ?>
