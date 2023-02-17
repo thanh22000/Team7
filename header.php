5cda59e1801f9820c9747d7532a91797ff0f0cdc
thanh22000 <onnellinentc@gmail.com>
2023-02-17 13:55:44 +0200

    re organize header.php

--- header.php
+++ header.php
@@ -129,50 +129,46 @@
         font-size: 18px;
       }
     </style>  
-    <div class="container">
-
-        <div class="row">
-            <div class="col-md-2" style="padding-top: 15px;">
-              <img class="img-fluid" src="Images/logonobg.png" alt="logo" style="height: 120px; width: 220px; " > 
-            </div>
-            <div class="col-md-10"> 
-
-    <h1 id="top">We bring the taste of Srilankan foods to Suomi </h1>
-
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid">
-      
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link active" href="http://localhost:81/Team7/index.php">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link active" href='#reservation'>Reservation</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link active dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Our Menu
-            </a>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="#srilankan">Srilankan</a></li>
-              <li><a class="dropdown-item" href="#indian">Indian</a></li>                  
-              <li><a class="dropdown-item" href="#chinese">Chinese</a></li>
-            </ul>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link active" href="#gallery">Gallery</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link active" href="#contact">Contact us</a>
-          </li>
-        </ul>
-        <a class="shoppingcart" href="#"> <i class="fa-solid fa-cart-shopping"></i> </a>
-        </div>
+    
+<!-- OUR PAGE -->   
+<div class="container">
+  <div class="row">
+    <div class="col-md-2" style="padding-top: 15px;">
+      <img class="img-fluid" src="Images/logonobg.png" alt="logo" style="height: 120px; width: 220px; " > 
     </div>
-  </nav>
-</div>
-</div>
+    <div class="col-md-10"> 
+      <h1 id="top">We bring the taste of Srilankan foods to Suomi </h1>
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" href="http://localhost:81/Team7/index.php">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" href='#reservation'>Reservation</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link active dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Our Menu</a>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="#srilankan">Srilankan</a></li>
+                  <li><a class="dropdown-item" href="#indian">Indian</a></li>                  
+                  <li><a class="dropdown-item" href="#chinese">Chinese</a></li>
+                </ul>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" href="#gallery">Gallery</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" href="#contact">Contact us</a>
+            </li>
+          </ul>
+            <a class="shoppingcart" href="#"> <i class="fa-solid fa-cart-shopping"></i> </a>
+        </div>
+      </div>
+      </nav>
+    </div>
+  </div>
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
