e40649294b5e20af6775368ab46a728cb7155f15
thanh22000 <onnellinentc@gmail.com>
2023-02-21 18:21:42 +0200

    Add to cart

--- mycart.php
+++ mycart.php
@@ -4,7 +4,11 @@ include 'cartdb.php';
 include 'header.php'; 
 ?>
 
-<h2> My cart </h2>
+<!-- Menu -->
+<h2 style="text-align: center">WHAT DO WE SERVE?</h2> <br>
+
+<!-- Srilankan food -->
+<h3 id="srilankan" style="text-align:center; color:aliceblue;">SRILANKAN MENU</h3><br>
 <?php 
 $sql = "Select * from tblfood where fid='1'";
 $result3 = $conn->query($sql);
@@ -18,7 +22,9 @@ if($result3 -> num_rows > 0){
                 $row[fcode] <br>
                 € $row[price] </p>
             <div class='cart-action'>
-                <input type='text' class='food-quantity' name='quantity' 
+                <input type='text' class='food-quantity' name='quantity' value='1' size='2' />
+                <input type='submit' value='Add to Cart' class='btnAddAction' />
+            </div>
         </div>
         ";
     }
@@ -33,14 +39,316 @@ if($result3 -> num_rows > 0){
             <p> $row[name] <br>
                 $row[fcode] <br>
                 € $row[price] </p>
-        </div>
+                <div class='cart-action'>
+                <input type='text' class='food-quantity' name='quantity' value='1' size='2' />
+                <input type='submit' value='Add to Cart' class='btnAddAction' />
+            </div>
+        </div> 
+        ";
+    }
+}
+$sql = "Select * from tblfood where fid='3'";
+$result3 = $conn->query($sql);
+if($result3 -> num_rows > 0){
+    while($row=$result3 -> fetch_assoc()) {
+        echo "
+        <div class='card' style='text-align: center; font-weight: bold; background-color: darkgray; width: 350px;'>
+            <img src='Images/srilankan3_milkrice.jpeg' alt='milkrice' style='height: 250px; width: 350px;'>
+            <p> $row[name] <br>
+                $row[fcode] <br>
+                € $row[price] </p>
+                <div class='cart-action'>
+                <input type='text' class='food-quantity' name='quantity' value='1' size='2' />
+                <input type='submit' value='Add to Cart' class='btnAddAction' />
+            </div>
+        </div> </div> <br>
+        ";
+    }
+}
+$sql = "Select * from tblfood where fid='4'";
+$result3 = $conn->query($sql);
+if($result3 -> num_rows > 0){
+    while($row=$result3 -> fetch_assoc()) {
+        echo "
+        <div class='menu'>
+        <div class='card' style='text-align: center; font-weight: bold; background-color: darkgray; width: 350px;'>
+            <img src='Images/srilankan4_roti.jpeg' alt='roti' style='height: 250px; width: 350px;'>
+            <p> $row[name] <br>
+                $row[fcode] <br>
+                € $row[price] </p>
+            <div class='cart-action'>
+                <input type='text' class='food-quantity' name='quantity' value='1' size='2' />
+                <input type='submit' value='Add to Cart' class='btnAddAction' />
+            </div>
         </div>
         ";
     }
 }
-
+$sql = "Select * from tblfood where fid='5'";
+$result3 = $conn->query($sql);
+if($result3 -> num_rows > 0){
+    while($row=$result3 -> fetch_assoc()) {
+        echo "
+        <div class='card' style='text-align: center; font-weight: bold; background-color: darkgray; width: 350px;'>
+            <img src='Images/srilankan5_hoppers.jpeg' alt='hoppers' style='height: 250px; width: 350px;'>
+            <p> $row[name] <br>
+                $row[fcode] <br>
+                € $row[price] </p>
+                <div class='cart-action'>
+                <input type='text' class='food-quantity' name='quantity' value='1' size='2' />
+                <input type='submit' value='Add to Cart' class='btnAddAction' />
+            </div>
+        </div> 
+        ";
+    }
+}
+$sql = "Select * from tblfood where fid='6'";
+$result3 = $conn->query($sql);
+if($result3 -> num_rows > 0){
+    while($row=$result3 -> fetch_assoc()) {
+        echo "
+        <div class='card' style='text-align: center; font-weight: bold; background-color: darkgray; width: 350px;'>
+            <img src='Images/srilankan6_riceandcurry.jpeg' alt='riceandcurry' style='height: 250px; width: 350px;'>
+            <p> $row[name] <br>
+                $row[fcode] <br>
+                € $row[price] </p>
+                <div class='cart-action'>
+                <input type='text' class='food-quantity' name='quantity' value='1' size='2' />
+                <input type='submit' value='Add to Cart' class='btnAddAction' />
+            </div>
+        </div> </div> <br>
+        ";
+    }
+}
 ?>
-
+<!-- Indian menu -->
+<h3 id="indian" style="text-align:center; color:aliceblue;">INDIAN MENU</h3><br>
+<?php 
+$sql = "Select * from tblfood where fid='7'";
+$result3 = $conn->query($sql);
+if($result3 -> num_rows > 0){
+    while($row=$result3 -> fetch_assoc()) {
+        echo "
+        <div class='menu'>
+        <div class='card' style='text-align: center; font-weight: bold; background-color: darkgray; width: 350px;'>
+            <img src='Images/indian1_ulunduwada.jpeg' alt='ulunduwada' style='height: 250px; width: 350px;'>
+            <p> $row[name] <br>
+                $row[fcode] <br>
+                € $row[price] </p>
+            <div class='cart-action'>
+                <input type='text' class='food-quantity' name='quantity' value='1' size='2' />
+                <input type='submit' value='Add to Cart' class='btnAddAction' />
+            </div>
+        </div>
+        ";
+    }
+}
+$sql = "Select * from tblfood where fid='8'";
+$result3 = $conn->query($sql);
+if($result3 -> num_rows > 0){
+    while($row=$result3 -> fetch_assoc()) {
+        echo "
+        <div class='card' style='text-align: center; font-weight: bold; background-color: darkgray; width: 350px;'>
+            <img src='Images/indian2_dhosa.jpeg' alt='dhosa' style='height: 250px; width: 350px;'>
+            <p> $row[name] <br>
+                $row[fcode] <br>
+                € $row[price] </p>
+                <div class='cart-action'>
+                <input type='text' class='food-quantity' name='quantity' value='1' size='2' />
+                <input type='submit' value='Add to Cart' class='btnAddAction' />
+            </div>
+        </div> 
+        ";
+    }
+}
+$sql = "Select * from tblfood where fid='9'";
+$result3 = $conn->query($sql);
+if($result3 -> num_rows > 0){
+    while($row=$result3 -> fetch_assoc()) {
+        echo "
+        <div class='card' style='text-align: center; font-weight: bold; background-color: darkgray; width: 350px;'>
+            <img src='Images/indian3_gheerice.jpeg' alt='gheerice' style='height: 250px; width: 350px;'>
+            <p> $row[name] <br>
+                $row[fcode] <br>
+                € $row[price] </p>
+                <div class='cart-action'>
+                <input type='text' class='food-quantity' name='quantity' value='1' size='2' />
+                <input type='submit' value='Add to Cart' class='btnAddAction' />
+            </div>
+        </div> </div> <br>
+        ";
+    }
+}
+$sql = "Select * from tblfood where fid='10'";
+$result3 = $conn->query($sql);
+if($result3 -> num_rows > 0){
+    while($row=$result3 -> fetch_assoc()) {
+        echo "
+        <div class='menu'>
+        <div class='card' style='text-align: center; font-weight: bold; background-color: darkgray; width: 350px;'>
+            <img src='Images/indian4_idly.jpeg' alt='idly' style='height: 250px; width: 350px;'>
+            <p> $row[name] <br>
+                $row[fcode] <br>
+                € $row[price] </p>
+            <div class='cart-action'>
+                <input type='text' class='food-quantity' name='quantity' value='1' size='2' />
+                <input type='submit' value='Add to Cart' class='btnAddAction' />
+            </div>
+        </div>
+        ";
+    }
+}
+$sql = "Select * from tblfood where fid='11'";
+$result3 = $conn->query($sql);
+if($result3 -> num_rows > 0){
+    while($row=$result3 -> fetch_assoc()) {
+        echo "
+        <div class='card' style='text-align: center; font-weight: bold; background-color: darkgray; width: 350px;'>
+            <img src='Images/indian5_parota.jpg' alt='parota' style='height: 250px; width: 350px;'>
+            <p> $row[name] <br>
+                $row[fcode] <br>
+                € $row[price] </p>
+                <div class='cart-action'>
+                <input type='text' class='food-quantity' name='quantity' value='1' size='2' />
+                <input type='submit' value='Add to Cart' class='btnAddAction' />
+            </div>
+        </div> 
+        ";
+    }
+}
+$sql = "Select * from tblfood where fid='12'";
+$result3 = $conn->query($sql);
+if($result3 -> num_rows > 0){
+    while($row=$result3 -> fetch_assoc()) {
+        echo "
+        <div class='card' style='text-align: center; font-weight: bold; background-color: darkgray; width: 350px;'>
+            <img src='Images/indian6_biriyani.jpeg' alt='biriyani' style='height: 250px; width: 350px;'>
+            <p> $row[name] <br>
+                $row[fcode] <br>
+                € $row[price] </p>
+                <div class='cart-action'>
+                <input type='text' class='food-quantity' name='quantity' value='1' size='2' />
+                <input type='submit' value='Add to Cart' class='btnAddAction' />
+            </div>
+        </div> </div> <br>
+        ";
+    }
+}
+?>
+<!-- Chinese menu -->
+<h3 id="chinese" style="text-align:center; color:aliceblue;">CHINESE MENU</h3><br>
+<?php 
+$sql = "Select * from tblfood where fid='13'";
+$result3 = $conn->query($sql);
+if($result3 -> num_rows > 0){
+    while($row=$result3 -> fetch_assoc()) {
+        echo "
+        <div class='menu'>
+        <div class='card' style='text-align: center; font-weight: bold; background-color: darkgray; width: 350px;'>
+            <img src='Images/chinese1_noodles.jpg' alt='noodles' style='height: 250px; width: 350px;'>
+            <p> $row[name] <br>
+                $row[fcode] <br>
+                € $row[price] </p>
+            <div class='cart-action'>
+                <input type='text' class='food-quantity' name='quantity' value='1' size='2' />
+                <input type='submit' value='Add to Cart' class='btnAddAction' />
+            </div>
+        </div>
+        ";
+    }
+}
+$sql = "Select * from tblfood where fid='14'";
+$result3 = $conn->query($sql);
+if($result3 -> num_rows > 0){
+    while($row=$result3 -> fetch_assoc()) {
+        echo "
+        <div class='card' style='text-align: center; font-weight: bold; background-color: darkgray; width: 350px;'>
+            <img src='Images/chinese2_friedrice.jpg' alt='friedrice' style='height: 250px; width: 350px;'>
+            <p> $row[name] <br>
+                $row[fcode] <br>
+                € $row[price] </p>
+                <div class='cart-action'>
+                <input type='text' class='food-quantity' name='quantity' value='1' size='2' />
+                <input type='submit' value='Add to Cart' class='btnAddAction' />
+            </div>
+        </div> 
+        ";
+    }
+}
+$sql = "Select * from tblfood where fid='15'";
+$result3 = $conn->query($sql);
+if($result3 -> num_rows > 0){
+    while($row=$result3 -> fetch_assoc()) {
+        echo "
+        <div class='card' style='text-align: center; font-weight: bold; background-color: darkgray; width: 350px;'>
+            <img src='Images/chinese3_riceamngosalad.jpg' alt='riceamngosalad' style='height: 250px; width: 350px;'>
+            <p> $row[name] <br>
+                $row[fcode] <br>
+                € $row[price] </p>
+                <div class='cart-action'>
+                <input type='text' class='food-quantity' name='quantity' value='1' size='2' />
+                <input type='submit' value='Add to Cart' class='btnAddAction' />
+            </div>
+        </div> </div> <br>
+        ";
+    }
+}
+$sql = "Select * from tblfood where fid='16'";
+$result3 = $conn->query($sql);
+if($result3 -> num_rows > 0){
+    while($row=$result3 -> fetch_assoc()) {
+        echo "
+        <div class='menu'>
+        <div class='card' style='text-align: center; font-weight: bold; background-color: darkgray; width: 350px;'>
+            <img src='Images/chinese4_sandwitch.jpg' alt='sandwitch' style='height: 250px; width: 350px;'>
+            <p> $row[name] <br>
+                $row[fcode] <br>
+                € $row[price] </p>
+            <div class='cart-action'>
+                <input type='text' class='food-quantity' name='quantity' value='1' size='2' />
+                <input type='submit' value='Add to Cart' class='btnAddAction' />
+            </div>
+        </div>
+        ";
+    }
+}
+$sql = "Select * from tblfood where fid='17'";
+$result3 = $conn->query($sql);
+if($result3 -> num_rows > 0){
+    while($row=$result3 -> fetch_assoc()) {
+        echo "
+        <div class='card' style='text-align: center; font-weight: bold; background-color: darkgray; width: 350px;'>
+            <img src='Images/chinese5_friedbeef.jpg' alt='friedbeef' style='height: 250px; width: 350px;'>
+            <p> $row[name] <br>
+                $row[fcode] <br>
+                € $row[price] </p>
+                <div class='cart-action'>
+                <input type='text' class='food-quantity' name='quantity' value='1' size='2' />
+                <input type='submit' value='Add to Cart' class='btnAddAction' />
+            </div>
+        </div> 
+        ";
+    }
+}
+$sql = "Select * from tblfood where fid='18'";
+$result3 = $conn->query($sql);
+if($result3 -> num_rows > 0){
+    while($row=$result3 -> fetch_assoc()) {
+        echo "
+        <div class='card' style='text-align: center; font-weight: bold; background-color: darkgray; width: 350px;'>
+            <img src='Images/chinese6_potatos.jpg' alt='potatos' style='height: 250px; width: 350px;'>
+            <p> $row[name] <br>
+                $row[fcode] <br>
+                € $row[price] </p>
+                <div class='cart-action'>
+                <input type='text' class='food-quantity' name='quantity' value='1' size='2' />
+                <input type='submit' value='Add to Cart' class='btnAddAction' />
+            </div>
+        </div> </div> <br>
+        ";
+    }
+}
+?>
             
 
 
