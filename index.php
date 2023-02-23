a2f320c9b66663bc4b3c40cf70c8b52579409de8
thanh22000 <onnellinentc@gmail.com>
2023-02-23 13:30:16 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php 
 $title = "Home Page";
-include 'header.php'; 
-include 'cartdb.php'; ?>
+include 'header.php';  ?>
 <!-- Home Page -->
 <div class="row">
   <div class="col-md-6" style="font-size: 65px; font-weight: bolder; text-align: center; font-family:cursive; color: aliceblue; padding-top: 50px;">  
